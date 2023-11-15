227f566fc2f1cfe22f1577ba1fa2548e004cdc75
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-11-15 21:02:43 +0200

    Add files via upload

---
+++ JS Arrays and Strings/Lab/task4Substring.js
@@ -0,0 +1,8 @@
+function solve(word, startIndex, countEl){
+let sub = word.substring(startIndex, startIndex + countEl);
+console.log(sub);
+
+}
+
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);
