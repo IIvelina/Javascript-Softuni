35df558e6203319e486c8c6abf794c82de920964
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-11-19 17:54:10 +0200

    Add files via upload

---
+++ JS Arrays and Strings/Exercise/task5RevealWords.js
@@ -0,0 +1,18 @@
+function solve(words, text){
+    let wordsArr = words.split(', ');
+    let textArr = text.split(' ');
+
+    for (let i = 0; i < wordsArr.length; i++) {
+    
+        for (let j = 0; j < textArr.length; j++) {
+            
+            if(textArr[j].includes('*') && textArr[j].length === wordsArr[i].length){
+                textArr[j] = wordsArr[i];
+            }
+        }
+    }
+    console.log(textArr.join(' '));
+}
+
+solve('great','softuni is ***** place for learning new programming languages');
+solve('great, learning','softuni is ***** place for ******** new programming languages');
