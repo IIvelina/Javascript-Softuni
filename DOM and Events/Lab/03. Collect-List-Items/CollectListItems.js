c2cfb5d190ff5b43051e9077fd2b35265699daa0
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-12-10 15:36:47 +0200

    Add files via upload

---
+++ DOM and Events/Lab/03. Collect-List-Items/CollectListItems.js
@@ -0,0 +1,10 @@
+function extractText() {
+    let liElements = Array.from(document.querySelectorAll('#items > li'));
+    let result = document.getElementById('result');
+
+   liElements
+   .forEach((li) => {
+    result.textContent += li.textContent + '\n';
+   });
+   
+}
