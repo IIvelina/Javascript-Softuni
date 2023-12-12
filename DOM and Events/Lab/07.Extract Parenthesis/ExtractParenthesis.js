1a329471ba77cb6f1f478ddde80160da6e7892d1
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-12-12 20:15:07 +0200

    Add files via upload

---
+++ DOM and Events/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -0,0 +1,18 @@
+function extract(id) {
+ 
+    let text = document.getElementById(id).textContent;
+
+    let pattern = (/\((.*?)\)/g);
+
+    let result = [];
+
+    let match = pattern.exec(text);
+    
+    while (match !== null) {
+    result.push(match[1]);
+    match = pattern.exec(text);
+    }
+
+    return result.join('; ');
+    }
+
